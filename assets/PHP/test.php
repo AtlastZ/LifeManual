69503dafaaf70bf1b5eaef6d26220b310ccade39
AnalyzyA <analyzya@gmail.com>
2023-12-09 01:46:27 +0700

    main

--- assets/PHP/test.php
+++ assets/PHP/test.php
@@ -5,6 +5,7 @@
 <?php
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
+echo phpinfo();
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?> 
