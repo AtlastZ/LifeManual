1af4d25e0b004f8388ac9807cb85b1fcd1a88ead
AnalyzyA <analyzya@gmail.com>
2023-12-08 14:06:21 +0700

    main

--- assets/PHP/sendDirect.php
+++ assets/PHP/sendDirect.php
@@ -1,16 +1,24 @@
 <?php
+// Include the PHPMailerAutoload.php file from the PHPMailer library
+// require 'path/to/PHPMailerAutoload.php'; // Adjust the path accordingly
+use PHPMailer\PHPMailer\PHPMailer;
+require '../vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the input text from the form
     $inputText = $_POST["inputText"];
+
+    // Get the sender's contact from the form
+    $senderContact = $_POST["senderContact"];
 
     // Set the target email address
     $to = "neolight4136@gmail.com";
 
     // Set the subject of the email
-    $subject = "New Form Submission!!";
+    $subject = "New Form Submission";
 
     // Compose the email message
-    $message = "Message : $inputText";
+    $message = "Message : \n$inputText\n\nSender Contact:\n$senderContact";
 
     // Handle file upload
     $fileTmpPath = $_FILES['fileInput']['tmp_name'];
@@ -26,16 +34,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message .= "\n\nAttached File: $destPath";
     }
 
-    // Set additional headers
-    $headers = "From: neolight41366@gmail.com\r\n";
-    $headers .= "Reply-To: neolight413666@gmail.com\r\n";
-    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer;
+
+    // Enable SMTP debugging (for testing)
+    // $mail->SMTPDebug = 2;
+
+    // Set mailer to use SMTP
+    $mail->isSMTP();
+
+    // Specify SMTP host
+    $mail->Host = 'smtp.gmail.com'; // Update with your SMTP server
+
+    // Enable SMTP authentication
+    $mail->SMTPAuth = true;
+
+    // SMTP username and password
+    $mail->Username = 'neolight7424@gmail.com'; // Update with your SMTP username
+    $mail->Password = '0819837424'; // Update with your SMTP password
+
+    // Set sender and recipient addresses
+    $mail->setFrom('neolight4136@gmail.com', 'Rei'); // Update with your email and name
+    $mail->addAddress($to);
+
+    // Set email subject and body
+    $mail->Subject = $subject;
+    $mail->Body = $message;
 
     // Send the email
-    if (mail($to, $subject, $message, $headers)) {
+    if ($mail->send()) {
         echo "Email sent successfully";
     } else {
-        echo "Error sending email";
+        echo "Error sending email: " . $mail->ErrorInfo;
     }
 } else {
     // Redirect to the form page if accessed directly without submission
